54d294c527fac3a284020f9e50f281e117c86845
vraj-desai <desaiv@umich.edu>
2019-10-07 23:22:42 -0400

    updated video_processing

--- video_processing.py
+++ video_processing.py
@@ -9,8 +9,6 @@ class video_processor:
         #Variable to select button status
         #TODO: Turn close webcam when button is off
         self.buttonStatus = False
-        self.currFrame = None
-
     
     def button_pressed():
         """Changes the status of button after pressed"""
@@ -20,6 +18,7 @@ class video_processor:
         if self.buttonStatus:
             cv2.destroyAllWindows()
             self.cam.release()
+            self.cam = None
         else:
             # CV2 video capture object
             self.cam = cv2.VideoCapture()
@@ -29,15 +28,31 @@ class video_processor:
             self.cam.set(4, 900)
 
     def process_frame():
-        """Reads in the frame and processes it"""
+        """Reads in the frame and processes it and display it"""
         #TODO: Read in the frame and process it
-        
-    
+        if self.cam != None:
+            readStatus, frame = self.cam.read()
+            #TODO: Process frame
+            cv2.imshow("attentionChecker", frame)
 
 
 def main():
     """Main function"""
     processor = video_processor()
+
+    # Button press will be the signal from hardware
+    # While button is on button_press will be True
+    # For now it is always true since there is no hardware signal
+    button_press = True
+
+    while True:
+        if button_press:
+            if not processor.buttonStatus:
+                processor.button_pressed()
+            processor.process_frame()
+        else:
+            processor.button_pressed()
+
 
     return
 
