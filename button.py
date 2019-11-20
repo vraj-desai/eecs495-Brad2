b1703e3729c8786d5803c072fa316e18871b5f2a
vraj-desai <desaiv@umich.edu>
2019-11-20 13:40:14 -0500

    Button + sound file

--- button.py
+++ button.py
@@ -1,16 +1,23 @@
 import RPi.GPIO as GPIO
+import os
 import object_detection_v2
 
 class Button:
     def __init__(self, pin):
         """Initialize the button with the GPIO pin."""
         self.pin = pin
+        
+
+try:
+    while True:
+        button = Button(7)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-while True:
-    button = Button(14)
-    GPIO.wait_for_edge(button.pin, GPIO.RISING)
-    object_detection_v2.object_detect(button.pin)
-    GPIO.cleanup()
+        GPIO.setup(button.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.wait_for_edge(button.pin, GPIO.RISING)
+        # os.system("vcgencmd display_power 1")
+        object_detection_v2.main()
+        # os.system("vcgencmd display_power 0")
+        GPIO.cleanup()
+except KeyboardInterrupt:
+    pass
+    # os.system("vcgencmd display_power 1")
